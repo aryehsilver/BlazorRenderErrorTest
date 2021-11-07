0fda134434d22e668fdca3a2af956b60a3a74afd
Aryeh Silver <aryehsilver@hotmail.com>
2021-11-07 03:54:28 +0000

    Add solution page

---
+++ BlazorRenderErrorTest/Pages/IndexWithRender.razor.cs
@@ -0,0 +1,30 @@
+﻿using BlazorRenderErrorTest.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorRenderErrorTest.Pages {
+  public partial class IndexWithRender {
+    [Inject]
+    private AppDbContext Context { get; set; }
+    private Person Person;
+    private List<Person> People = new();
+    private int PersonId;
+    private bool Render;
+
+    protected override async Task OnInitializedAsync() {
+      using (AppDbContext context = new()) {
+        context.Database.Migrate();
+      }
+      People = await Context.People.ToListAsync();
+      await Task.Delay(1000);
+      Person = new() {
+        Id = 1,
+        Name = "New person",
+        Tel = 05654534674
+      };
+      Render = true;
+    }
+  }
+}
