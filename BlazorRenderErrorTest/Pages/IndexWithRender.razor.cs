b97b8cb856665ffc051a7d865bfe46b654524ad0
Aryeh Silver <aryehsilver@hotmail.com>
2022-01-22 21:12:12 +0000

    Set person to first of people and fix naming violation

--- BlazorRenderErrorTest/Pages/IndexWithRender.razor.cs
+++ BlazorRenderErrorTest/Pages/IndexWithRender.razor.cs
@@ -2,29 +2,26 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlazorRenderErrorTest.Pages {
   public partial class IndexWithRender {
     [Inject]
     private AppDbContext Context { get; set; }
-    private Person Person;
-    private List<Person> People = new();
-    private int PersonId;
-    private bool Render;
+    private Person Person { get; set; }
+    private List<Person> _people = new();
+    private int _personId;
+    private bool _render;
 
     protected override async Task OnInitializedAsync() {
       using (AppDbContext context = new()) {
         context.Database.Migrate();
       }
-      People = await Context.People.ToListAsync();
+      _people = await Context.People.ToListAsync();
       await Task.Delay(1000);
-      Person = new() {
-        Id = 1,
-        Name = "New person",
-        Tel = 05654534674
-      };
-      Render = true;
+      Person = _people.FirstOrDefault();
+      _render = true;
     }
   }
 }
